6df02369a5b14d26b5bdbd24223634c3fdaf5d0c
lebrancconvas <lebrancconvas@gmail.com>
2022-12-24 12:54:12 +0700

    ✨ feat: create test page in nuxt project

---
+++ app/pages/test/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h2 v-if="isMessageShow">{{ message }}</h2>
+    <h2 v-else>{{ errorMessage }}</h2>
+    <v-btn @click="changeStatus">
+      Change Status
+    </v-btn>
+    <h3>
+      Member:
+      <p v-for="(member, index) in members" :key="index">
+        {{ member }}
+      </p>
+    </h3>
+    <h3>
+      Instant Message: {{ instantMessage }}
+      <v-text-field v-model="instantMessage"></v-text-field>
+    </h3>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        message: "Ikigai",
+        errorMessage: "Unknown Data",
+        isMessageShow: false,
+        members: [
+          "Jan",
+          "SomSom",
+          "Mint"
+        ],
+        instantMessage: "Hello World"
+      }
+    },
+    methods: {
+      changeStatus() {
+        this.isMessageShow = !this.isMessageShow;
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
