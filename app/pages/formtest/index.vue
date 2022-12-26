52d45d2ab3663b4441ba353447eeb4496cd846c6
lebrancconvas <lebrancconvas@gmail.com>
2022-12-26 12:11:22 +0700

    ✨ feat: create form test

---
+++ app/pages/formtest/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <v-form>
+      <v-text-field v-model="name" label="name"></v-text-field>
+      <v-text-field v-model="price" label="price"></v-text-field>
+      <v-btn @click="submitLog">Submit</v-btn>
+    </v-form>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        name: "",
+        price: ""
+      }
+    },
+    methods: {
+      submitLog() {
+        console.log(`Name: ${this.name}\nPrice: ${this.price}`)
+      }
+    }
+  }
+</script>
+
+<style lang="css" scoped>
+
+</style>
